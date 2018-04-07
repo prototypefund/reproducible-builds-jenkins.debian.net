54c5b6c7c47284f34074de644865135dddd3b7c8
Mattia Rizzolo <mattia@debian.org>
2018-04-07 23:06:04 +0200

    reproducible debian: missed a sql select after the last schema update...
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -433,7 +433,7 @@ call_diffoscope_on_changes_files() {
 
 choose_package() {
 	local RESULT=$(query_db "
-		SELECT s.suite, s.id, s.name, s.version, sch.save_artifacts, sch.notify, s.notify_maintainer, sch.message, sch.date_scheduled
+		SELECT s.suite, s.id, s.name, s.version, sch.save_artifacts, sch.notify, s.notify_maintainer, sch.date_scheduled
 		FROM schedule AS sch JOIN sources AS s ON sch.package_id=s.id
 		WHERE sch.date_build_started is NULL
 		AND s.architecture='$ARCH'
