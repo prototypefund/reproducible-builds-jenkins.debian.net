12c4b09133c1324142b79df2fcf45340f7cb1e50
Holger Levsen <holger@layer-acht.org>
2018-09-10 19:12:45 +0200

    reproducible Debian: use bpo kernels on pb5+15 to systematically vary the kernel on amd64 again
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- update_jdn.sh
+++ update_jdn.sh
@@ -472,7 +472,7 @@ if [ -f /etc/debian_version ] ; then
 		# - we use bpo kernels on pb-build5+15 (and the default i386 kernel on pb-build2+12-i386)
 		# - we use the default amd64 kernel on pb-build1+11 (and the default amd64 kernel on pb-build6+16-i386)
 		if [ "$HOSTNAME" = "profitbricks-build5-amd64" ] || [ "$HOSTNAME" = "profitbricks-build15-amd64" ] ; then
-			$UP2DATE || sudo apt-get install linux-image-amd64
+			$UP2DATE || sudo apt-get install -t stretch-backports linux-image-amd64
 		elif [ "$HOSTNAME" = "profitbricks-build6-i386" ] || [ "$HOSTNAME" = "profitbricks-build16-i386" ] ; then
 			$UP2DATE || sudo apt-get install linux-image-amd64
 		fi
