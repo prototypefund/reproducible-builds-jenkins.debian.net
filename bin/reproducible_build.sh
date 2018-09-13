7aac3a7707f7f4a9dacffa54eeadcb4da2d60ebe
Holger Levsen <holger@layer-acht.org>
2018-09-13 12:05:08 +0200

    reproducible Debian: ignore false positive disk full problem when building klibc
    the offending/ok regex is
    	'^ENOSPC \(.*\) => "No space left on device"'
    (coming from test output of klibc...)
    
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_build.sh
+++ bin/reproducible_build.sh
@@ -293,7 +293,7 @@ handle_ftbfs() {
 		done
 		# notify about unkown diskspace issues where we are not 100% sure yet those are diskspace issues
 		# ignore syslinux and clisp, which are false positives…
-		if zgrep -e "No space left on device" "$DEBIAN_BASE/logs/$SUITE/$ARCH/${SRCPACKAGE}_${EVERSION}.build${BUILD}.log.gz" && [ "$SRCPACKAGE" != "syslinux" ] && [ "$SRCPACKAGE" != "clisp" ] ; then
+		if zgrep -e "No space left on device" "$DEBIAN_BASE/logs/$SUITE/$ARCH/${SRCPACKAGE}_${EVERSION}.build${BUILD}.log.gz" && [ "$SRCPACKAGE" != "syslinux" ] && [ "$SRCPACKAGE" != "clisp" ] && [ "$SRCPACKAGE" != "klibc" ] ; then
 			MESSAGE="${BUILD_URL}console.log for ${SRCPACKAGE} (ftbfs in $SUITE/$ARCH) _probably_ had a diskspace issue on $node. Please check, tune handle_ftbfs() and reschedule the package."
 			echo $MESSAGE | tee -a /var/log/jenkins/reproducible-diskspace-issues.log
 			irc_message debian-reproducible "$MESSAGE"
