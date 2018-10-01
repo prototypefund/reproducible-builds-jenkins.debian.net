93c3a7ad52977b5b49ccf93b8b16375eb107232e
Holger Levsen <holger@layer-acht.org>
2018-10-01 19:49:08 +0200

    reproducible: make a distinction between systems automatically and manually marked offline in node health overview
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_html_nodes_info.sh
+++ bin/reproducible_html_nodes_info.sh
@@ -73,11 +73,14 @@ build_nodes_health_page() {
 			BADGE="$URL/badge/icon"
 			write_page "<td><a href='$URL'><img src='$BADGE' /></a></td>"
 			# mark offline nodes
-			if [ -f "$JENKINS_OFFLINE_LIST" ]; then
-				if grep -q "$NODE" "$JENKINS_OFFLINE_LIST"; then
-					write_page '</td><td colspan="10" style="text-align: center;"><span style="font-style: italic;">offline</span></td>'
-					continue
-				fi
+			JENKINS_OFFLINE_GIT_LIST=~jenkins-adm/jenkins.debian.net/jenkins-home/offline_nodes
+			if [ -f "$JENKINS_OFFLINE_GIT_LIST" ] && grep -q "$NODE" "$JENKINS_OFFLINE_GIT_LIST"; then
+				write_page '</td><td colspan="10" style="text-align: center;"><span style="font-style: italic;">temporarily marked offline by jenkins</span></td>'
+				continue
+
+			elif [ -f "$JENKINS_OFFLINE_LIST" ] && grep -q "$NODE" "$JENKINS_OFFLINE_LIST"; then
+				write_page '</td><td colspan="10" style="text-align: center;"><span style="font-style: italic;">offline</span></td>'
+				continue
 			fi
 			# maintenance
 			URL="https://jenkins.debian.net/view/reproducible/view/Node_maintenance/job/reproducible_maintenance_${ARCH}_${JENKINS_NODENAME}"
