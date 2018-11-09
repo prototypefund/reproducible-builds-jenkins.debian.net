7902f640d98a55314c21808879b50a3a73d9de16
Holger Levsen <holger@layer-acht.org>
2018-11-09 14:07:12 +0100

    reproducible Debian: document that we vary by installing the usrmerge package
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- bin/reproducible_common.sh
+++ bin/reproducible_common.sh
@@ -474,6 +474,7 @@ write_variation_table() {
 		write_page "<tr><td>uid</td><td>uid=1111</td><td>uid=2222</td></tr>"
 		write_page "<tr><td>gid</td><td>gid=1111</td><td>gid=2222</td></tr>"
 		write_page "<tr><td>/bin/sh</td><td>/bin/dash</td><td>/bin/bash</td></tr>"
+		write_page "<tr><td><em>usrmerge</em> package installed</td><td>no</td><td>yes</td></tr>"
 		write_page "<tr><td>build path</td><td>/build/1st/\$pkg-\$ver <em>(not varied for stretch/buster)</em></td><td>/build/\$pkg-\$ver/2nd <em>(not varied for stretch/buster)</em></td></tr>"
 		write_page "<tr><td>user's login shell</td><td>/bin/sh</td><td>/bin/bash</td></tr>"
 		write_page "<tr><td>user's <a href="https://en.wikipedia.org/wiki/Gecos_field">GECOS</a></td><td>first user,first room,first work-phone,first home-phone,first other</td><td>second user,second room,second work-phone,second home-phone,second other</td></tr>"
