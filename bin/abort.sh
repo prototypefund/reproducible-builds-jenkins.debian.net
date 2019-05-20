923d22a2b58d6c09bbd82e47f415a1f8400e5828
Mattia Rizzolo <mattia@debian.org>
2019-05-20 21:45:07 +0200

    abort.sh: make the script pass shellcheck, and use a trap to remove the temp file
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- bin/abort.sh
+++ bin/abort.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 # generally interesting: BUILD_* JENKINS_* JOB_* but most is in BUILD_URL, so:
-export | egrep "(BUILD_URL=)"
+export | grep -E "(BUILD_URL=)"
 TMPFILE=$(mktemp)
+trap 'rm "$TMPFILE"' EXIT
 
-curl https://jenkins.debian.net/jnlpJars/jenkins-cli.jar -o $TMPFILE
-java -jar $TMPFILE -s http://localhost:8080/ set-build-result aborted
-rm $TMPFILE
-exit
+curl https://jenkins.debian.net/jnlpJars/jenkins-cli.jar -o "$TMPFILE"
+java -jar "$TMPFILE" -s http://localhost:8080/ set-build-result aborted
