prototypefund/reproducible-builds-jenkins.debian.net

commit 7a0295e8ef9dd28c2250b2d418f8b279b875929e

reproducible archlinux: normalize the suite names in the database.
Signed-off-by: Mattia Rizzolo <mattia@debian.org>

bin/reproducible_archlinux_scheduler.sh
@@ -69,7 +69,7 @@ update_archlinux_repositories() {
 					REMOVE_LIST="$REMOVE_LIST $REPO/$PKG"
 					rm -r --one-file-system $BASE/archlinux/$REPO/$PKG
 					echo "$(date -u) - $REPO/$PKG removed as it's gone from the Archlinux repositories."
-					SUITE="archlinux_$REPO"
+					SUITE="$REPO"
 					PKG_ID=$(query_db "SELECT id FROM sources WHERE distribution=$DISTROID AND name='$PKG' AND suite='$SUITE' AND architecture='$ARCH';")
 					if [ -n "${PKG_ID}" ] ; then
 						query_db "DELETE FROM results WHERE package_id='${PKG_ID}';"
@@ -100,7 +100,7 @@ update_archlinux_repositories() {
 		grep "^$REPO" "$ARCHLINUX_PKGS"_full_pkgbase_list | \
 			while read repository pkgbase version; do
 				PKG=$pkgbase
-				SUITE="archlinux_$REPO"
+				SUITE="$REPO"
 				PKG_IN_DB=$(grep "^archlinux_$REPO|$pkgbase|" $KNOWN | head -1) # why oh why is head -1 needed here?
 				VERSION=$(echo ${PKG_IN_DB} | cut -d "|" -f3)
 			        if [ -z "${PKG_IN_DB}" ] ; then

bin/reproducible_archlinux_manual_scheduler.sh
@@ -23,7 +23,7 @@ fi
 
 DATE="$(date -u +'%Y-%m-%d %H:%M' -d '21 days ago')"
 REPOSITORY=$1
-SUITE=archlinux_$REPOSITORY
+SUITE=$REPOSITORY
 ARCH=x86_64
 shift
 PACKAGES="$@"

bin/reproducible_build_archlinux_pkg.sh
@@ -36,7 +36,7 @@ update_pkg_in_db() {
 	BUILD_DURATION="$(cat pkg.build_duration)"
 	BUILD_STATE=$(cat pkg.state)
 	BUILD_VERSION="$(cat pkg.version)"
-	SUITE="archlinux_$REPOSITORY"
+	SUITE="$REPOSITORY"
 	local SRCPKGID=$(query_db "SELECT id FROM sources WHERE distribution=$DISTROID AND name='$SRCPACKAGE' AND suite='$SUITE' AND architecture='$ARCH';")
 	if [ -z "${SRCPKGID}" ] ; then
 	        echo "${SRCPKGID} empty, ignoring $REPOSITORY/$SRCPACKAGE, failing hard."

bin/reproducible_db_maintenance.py
@@ -739,6 +739,16 @@ schema_updates = {
             "stats_notes", "stats_pkg_state",
         )
     ],
+    48: [  # normalize archlinux's suite names
+        "UPDATE sources SET suite='core' WHERE suite='archlinux_core'",
+        "UPDATE sources SET suite='extra' WHERE suite='archlinux_extra'",
+        "UPDATE sources SET suite='community' WHERE suite='archlinux_community'",
+        "UPDATE sources SET suite='multilib' WHERE suite='archlinux_multilib'",
+        "UPDATE stats_build SET suite='core' WHERE suite='archlinux_core'",
+        "UPDATE stats_build SET suite='extra' WHERE suite='archlinux_extra'",
+        "UPDATE stats_build SET suite='community' WHERE suite='archlinux_community'",
+        "UPDATE stats_build SET suite='multilib' WHERE suite='archlinux_multilib'",
+    ],
 }
 
 

bin/reproducible_html_archlinux.sh
@@ -53,7 +53,7 @@ repostats(){
 	for REPOSITORY in $ARCHLINUX_REPOS ; do
 		echo "$(date -u) - starting to analyse build results for '$REPOSITORY'."
 		# prepare stats per repository
-		SUITE="archlinux_$REPOSITORY"
+		SUITE="$REPOSITORY"
 		TOTAL=$(query_db "SELECT count(*) FROM sources AS s WHERE s.distribution=$DISTROID AND s.architecture='x86_64' AND s.suite='$SUITE';")
 		TESTED=$(query_db "SELECT count(*) FROM sources AS s JOIN results AS r ON s.id=r.package_id WHERE s.distribution=$DISTROID AND s.architecture='x86_64' AND s.suite='$SUITE';")
 		NR_GOOD=$(query_db "SELECT count(*) FROM sources AS s JOIN results AS r ON s.id=r.package_id WHERE s.distribution=$DISTROID AND s.architecture='x86_64' AND s.suite='$SUITE' AND r.status='reproducible';")

commit 231884e85792706cc80b758bdb4cc584cf1f1adf

reproducible archlinux: fix suite name also here
another fallout from 7a0295e8ef9dd28c2250b2d418f8b279b875929e

Signed-off-by: Mattia Rizzolo <mattia@debian.org>

bin/reproducible_archlinux_scheduler.sh
@@ -101,7 +101,7 @@ update_archlinux_repositories() {
 			while read repository pkgbase version; do
 				PKG=$pkgbase
 				SUITE="$REPO"
-				PKG_IN_DB=$(grep "^archlinux_$REPO|$pkgbase|" $KNOWN | head -1) # why oh why is head -1 needed here?
+				PKG_IN_DB=$(grep "^$REPO|$pkgbase|" $KNOWN | head -1) # why oh why is head -1 needed here?
 				VERSION=$(echo ${PKG_IN_DB} | cut -d "|" -f3)
 			        if [ -z "${PKG_IN_DB}" ] ; then
 					# new package, add to db and schedule